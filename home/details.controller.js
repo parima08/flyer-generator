d9b72820486a9cad5eed0d569d8e5b622a80126e
Parima Shah <Parima08@gmail.com>
2017-05-01 17:17:41 +0530

    Detail Page working

--- home/details.controller.js
+++ home/details.controller.js
@@ -27,62 +27,83 @@ function DetailsController($scope, $routeParams, $location,
 		});
 	}); 
 
+	$scope.submitForm = function(){
+		console.log("submitForm"); 
+		var values = {};
+		$.each($('#myForm').serializeArray(), function(i, field) {
+		    values[field.name] = field.value;
+		});
+
+		drawImageScaled(values, img, context)
+		e.preventDefault(); 
+	}
+
    	var canvasSetup = function(){
    		var canvas = $("#canvas")[0];
 		console.log(canvas); 
-		var context = canvas.getContext('2d');
-		context.imageSmoothingEnabled = false;
+		//var context = canvas.getContext('2d');
+		//context.imageSmoothingEnabled = false;
 		var img = new Image();
 		values = {date: "hello", time: "yay", location1: "", location2: "", location3: ""}
+		resizeCanvas(canvas); ; 
 		img.onload = function(){
-	         drawImageScaled(values, img, context)
+	         drawImageScaled(values, img, canvas)
 	     };
 	    img.src = $scope.object.imageLink; 
 	}
 
-	var drawImageScaled = function(values, img, ctx) {
-	   	var canvas = ctx.canvas ;
+	var drawImageScaled = function(values, img, canvas) {
 	   	image_ratio = img.height/img.width; 
-	   	canvas_width_shld_be = 500; 
-	   	canvas_height_shld_be = image_ratio * canvas_width_shld_be; 
-	   	ctx.canvas.width = canvas_width_shld_be; 
-	   	ctx.canvas.height = canvas_height_shld_be; 
-		ctx.imageSmoothingEnabled = false;
-		ctx.drawImage(img, 0,0, canvas.width, canvas.height)
+	   	//scale_ratio = 2; 
+	   	//canvas_width_shld_be = 500; 
+	   	// canvas_height_shld_be = image_ratio * canvas_width_shld_be; 
+	   	// canvas.style.width = canvas_width_shld_be + "px"; 
+	   	// canvas.style.height = canvas_height_shld_be + "px"; 
+	   	// canvas.width = canvas_width_shld_be * scale_ratio; 
+	   	// canvas.height = canvas_height_shld_be * scale_ratio; 
+	   	ctx = canvas.getContext('2d'); 
+	   	ctx.scale(scale_ratio, scale_ratio); 
+		ctx.imageSmoothingEnabled = true;
+		ctx.drawImage(img, 0,0, img.width, img.height, 0, 0, canvas.width, canvas.height)
 
 		for(var i = 0; i <  $scope.formInfo.length; i++) {
 			field = $scope.formInfo[i]; 
-			ctx.font = field.fontSize + " " + field.font; 
+			var fontSize = parseInt(field.fontSize) * 4; 
+			console.log(fontSize); 
+			ctx.font = fontSize.toString() + "pt " + field.font; 
 			ctx.fillStyle = field.fontColor; 
 			ctx.textAlign = field.textAlign; 
 			positionXY = field.position.split(','); 
-			positionX = positionXY[0]; 
-			positionY = positionXY[1]; 
+			positionX = positionXY[0] * 4; 
+			positionY = positionXY[1] * 4; 
 			ctx.fillText(values[ctx.name], positionX, positionY)
 		}
-		// //DATE
-		// ctx.font = "12pt Noto Serif";
-		// ctx.fillStyle = GOLD;
-		// ctx.textAlign = "right";
-		// ctx.fillText(values[DATE], canvas.width - 212, canvas.height - 212, 400);
-	  	
-		// //TIMINGS
-		// ctx.font = "12pt Noto Serif";
-		// ctx.fillStyle = "white"
-		// ctx.textAlign = "left";
-		// ctx.fillText(values[TIME], canvas.width - 200, canvas.height - 212, 400);
-
-		// //LOCATION1
-		// ctx.font = "12pt Noto Serif";
-		// ctx.fillStyle = "white"
-		// ctx.textAlign = "right";
-		// ctx.fillText(values[LOCATION1], canvas.width - 50, canvas.height - 170, 400);
-		// //LOCATION2
-		// ctx.fillText(values[LOCATION2], canvas.width - 50, canvas.height - 150, 400);
-
-		// //LOCATION3
-		// ctx.fillText(values[LOCATION3], canvas.width - 50, canvas.height - 130, 400);
-
-
 	}
+
+	var resizeCanvas = function(canvas){
+		canvas_width_shld_be= 500; 
+		canvas_height_shld_be = 693;
+		scale_ratio = 4;  
+		//canvas_height_shld_be = image_ratio * canvas_width_shld_be; 
+	   	canvas.style.width = canvas_width_shld_be + "px"; 
+	   	canvas.style.height = canvas_height_shld_be + "px"; 
+	   	canvas.width = canvas_width_shld_be * scale_ratio; 
+	   	canvas.height = canvas_height_shld_be * scale_ratio;
+	   	ctx = canvas.getContext('2d'); 
+	   	ctx.scale(.25, .25);
+	   	//ctx.scale(scale_ratio, scale_ratio);  
+	}
+
+	// var changeResolution = function(canvas, scaleFactor) {
+	//     // Set up CSS size if it's not set up already
+	//     if (!canvas.style.width)
+	//         canvas.style.width = canvas.width + 'px';
+	//     if (!canvas.style.height)
+	//         canvas.style.height = canvas.height + 'px';
+
+	//     canvas.width = Math.ceil(canvas.width * scaleFactor);
+	//     canvas.height = Math.ceil(canvas.height * scaleFactor);
+	//     var ctx = canvas.getContext('2d');
+	//     ctx.scale(scaleFactor, scaleFactor);
+	// }
 }
