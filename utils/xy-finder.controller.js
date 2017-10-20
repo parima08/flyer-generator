8196d6fe5d8d13fbca016fa4c3510f6bd7937802
Parima Shah <Parima08@gmail.com>
2017-10-20 17:36:03 +0530

    Updated the XY Coords tool

--- utils/xy-finder.controller.js
+++ utils/xy-finder.controller.js
@@ -2,23 +2,30 @@
 var app = angular.module('myApp')
 app.controller("XyCoordController", XyCoordController); 
 
-XyCoordController.$inject = ['$scope', '$rootScope']; 
+XyCoordController.$inject = ['$scope', '$rootScope', 'subpageDetails', 'objectDetailsService']; 
 
-function XyCoordController($scope){
+function XyCoordController($scope, $rootScope, subpageDetails, objectDetailsService){
 	console.log("XyCoodController"); 
+	$scope.sizeOptionsNames =  Object.keys(subpageDetails); 
+
 	$scope.submitImage = function(){
 		console.log("Submitting Image"); 
 		var formArray = $('#xyform').serializeArray(); 
 		console.log(formArray); 
 		var imageLink = formArray[1].value; 
-		var typeOfFile = formArray[0].value; 
-		if(typeOfFile == "banner"){
-			$('.imageFindXy').width(834); 
-		}
-		else{
-			//assumes that it is a flyer
-			$('.imageFindXy').width(500)
-		}
+		console.log(formArray[0]['name']); 
+		var typeOfFile = formArray[0]['value'];
+		var fileWidth = subpageDetails[typeOfFile].width; 
+		var fileHeight = subpageDetails[typeOfFile].height;
+		var dimensions = objectDetailsService.calculateAssetSize(fileWidth, fileHeight); 
+		$('.imageFindXy').width(dimensions.canvasWidth); 
+		// if(typeOfFile == "banner"){
+		// 	$('.imageFindXy').width(834); 
+		// }
+		// else{
+		// 	//assumes that it is a flyer
+		// 	$('.imageFindXy').width(500)
+		// }
 		console.log(imageLink); 
 		console.log(typeOfFile); 
 		var img = new Image(); 
