958ceeed2a3f481a9c03098de88fcdf95b965509
Parima Shah <parima@outschool.com>
2022-02-05 15:06:06 -0800

    Updated link to use https rather than http

--- home/home.controller.js
+++ home/home.controller.js
@@ -159,7 +159,8 @@ app.service("objectDetailsService", function ($http, $q, $sce) {
 			canvasHeight: dimensions['canvasHeight'],
 			canvasWidth: dimensions['canvasWidth'],
 			imageLink: `https://s3.amazonaws.com/srmd-flyer-generator/${awsLinkPath}`,
-			thumbnailLink: `http://srmd-flyer-generator.s3-website-us-east-1.amazonaws.com/${thumbnailWidth}x${thumbnailHeight}/${awsLinkPath}`,
+			thumbnailLink: `https://s3.amazonaws.com/srmd-flyer-generator/${thumbnailWidth}x${thumbnailHeight}/${awsLinkPath}`,
+			//thumbnailLink: `http://srmd-flyer-generator.s3-website-us-east-1.amazonaws.com/${thumbnailWidth}x${thumbnailHeight}/${awsLinkPath}`,
 			worksheetIndex: el[map.worksheetIndex],
 			language: el[map.language].toLowerCase(),
 		}
