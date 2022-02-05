add96104d63272321ca4e584b6f1f34130e282e5
Parima Shah <parima@outschool.com>
2022-02-05 14:29:41 -0800

    Added resize lambda code

---
+++ resize-aws-lambda/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3({
+  signatureVersion: 'v4',
+});
+const Sharp = require('sharp');
+
+const BUCKET = process.env.BUCKET;
+const URL = process.env.URL;
+
+exports.handler = function(event, context, callback) {
+  const key = event.queryStringParameters.key;
+  const match = key.match(/(\d+)x(\d+)\/(.*)/);
+  const width = parseInt(match[1], 10);
+  const height = parseInt(match[2], 10);
+  const originalKey = match[3];
+
+  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+    .then(data => Sharp(data.Body)
+      .resize(width, height)
+      .toFormat('png')
+      .toBuffer()
+    )
+    .then(buffer => S3.putObject({
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: 'image/png',
+        Key: key,
+      }).promise()
+    )
+    .then(() => callback(null, {
+        statusCode: '301',
+        headers: {'location': `${URL}/${key}`},
+        body: '',
+      })
+    )
+    .catch(err => callback(err))
+}
